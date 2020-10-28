24472994be5c668162f303265b99df46a3037a3d
Joshua Mclean <joshmcleana@gmail.com>
2020-10-28 12:41:27 -0700

    Create and write initial notes for solution

---
+++ ProblemSolvingPatterns/SlidingWindow/minSubArrayLength.js
@@ -0,0 +1,14 @@
+/* Write a function called minSubArrayLen which accepts two parameters 
+- an array of positive integers and a positive integer.
+
+This function should return the MINIMAL LENGTH of a CONTIGUOUS SUBARRAY 
+of which the SUM is >= the INTEGER PASSED TO THE FUNCTION. 
+If there isn't one, return zero instead.
+
+My Words: This function needs to target the smallest subarray of values that
+when each value is added together, the sum is equal to the number that is passed in
+if there is any single value greater than the number passed in, return 1
+if no subarrays are found, return 0 <--- solve this first and the rest will come.
+
+
+*/
