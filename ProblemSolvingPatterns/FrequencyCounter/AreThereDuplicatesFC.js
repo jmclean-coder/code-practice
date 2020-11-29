6c0bf59c13821ae3302aacecc2d97eb93455a7fe
Joshua Mclean <joshmcleana@gmail.com>
2020-11-29 15:23:25 -0800

    Optimize solution

--- ProblemSolvingPatterns/FrequencyCounter/AreThereDuplicatesFC.js
+++ ProblemSolvingPatterns/FrequencyCounter/AreThereDuplicatesFC.js
@@ -14,10 +14,12 @@ function areThereDuplicates() {
   }
 
   let frequencyCounter = {};
-  for (let val in arguments) {
-    console.log(val);
-    frequencyCounter[arguments[val]] =
-      (frequencyCounter[arguments[val]] || 0) + 1;
+  console.log(arguments)
+  for (let index in arguments) {
+    // val is the index
+    console.log(index);
+    frequencyCounter[arguments[index]] =
+      (frequencyCounter[arguments[index]] || 0) + 1;
   }
 
   console.log(frequencyCounter);
@@ -30,7 +32,43 @@ function areThereDuplicates() {
   return false;
 }
 
-areThereDuplicates(1, 2, 3, 4, 4); // true
-areThereDuplicates("a", "b", "c", "d", "d"); // true
-areThereDuplicates(1, 2, 3); // false
-areThereDuplicates(); // null
+// function areThereDuplicates() {
+//   //edge cases
+//   if (arguments.length === 0) {
+//     return null;
+//   }
+//   if (arguments.length === 1) {
+//     return false;
+//   }
+//   // Make a counter object to track frequency of argument occurence
+//   let counter = {};
+//   // iterate over all arguments, for each argument:
+//   // if the argument doesn't exist as a key in the counter, create
+//   // that argument's key at set it's value to 0 OR increment it by 1
+//   for (let i = 0; i < arguments.length; i++) {
+//     // console.log(arguments[i]);
+//     if (counter[arguments[i]] === undefined) {
+//       counter[arguments[i]] = 1;
+//     } else {
+//       counter[arguments[i]] = counter[arguments[i]] + 1;
+//     }
+//   }
+//   // iterate over the counter object, for each key:
+//   // if the key's value is greater than 1
+//   // return true
+
+//   let keys = Object.keys(counter)
+//   for (let i = 0; i < keys.length; i++){
+//     if (counter[keys[i]] > 1) {
+//       return true;
+//     }
+//   }
+//   //otherwise:
+//   //return false
+//   return false
+// }
+
+console.log(areThereDuplicates(1, 2, 3, 4, 4)); // true
+console.log(areThereDuplicates("a", "b", "c", "d", "d")); // true
+console.log(areThereDuplicates(1, 2, 3)); // false
+console.log(areThereDuplicates()); // null
